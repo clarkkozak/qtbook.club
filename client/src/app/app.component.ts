018faa1c366930c40f8ccb5e3e001594b73edb6a
juxley <juxley@foxy>
2018-03-09 00:46:25 -0500

    Integrated qbook.club HTML and CSS into Angular
    Here I've created a component call nav-bar and add the HTML and CSS. I will have to edit the styles of the navigation and also change it so it stays with the component. I've added the nav-bar component to app.component.html as well. I believe I can also make the navigation be more appendble.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+import {book, author} from './test';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+    title = "Quantum Book Club";
+    book = book;
+    author = author;
 }
